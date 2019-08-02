6cbb5ca362ca4fd4840b31ed9644eb1f9e81b3a7
zhy <15633936860@163.com>
2019-08-02 18:31:43 +0800

    socket

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -20,18 +20,32 @@ class Index extends Controller
 
     public  function  main(){
 
+        //客户端代码
+        $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+        $msg1 = '你好和豆腐干';
+        //转为GBK编码，处理乱码问题
+        $msg = mb_convert_encoding($msg1,'GBK','UTF-8');
+        $len = strlen($msg);
+        socket_sendto($sock, $msg, $len, 0, '192.168.1.96', 12343);
+        socket_close($sock);
     }
 
     public  function  socket(){
-
-        $address = '192.168.1.96';
-        $port = '6640';
-        $this->master = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)     or die("socket_create() failed");
+        //服务器代码
+        $address ='' ;
+        $port = '12341';
+        //创建socket套接字
+        $this->master = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)     or die("socket_create() failed");
         socket_set_option($this->master, SOL_SOCKET, SO_REUSEADDR, 1)  or die("socket_option() failed");
-        socket_bind($this->master, $address, $port)                    or die("socket_bind() failed");
-        socket_listen($this->master,20)                                or die("socket_listen() failed");
-
-        print_r($this->master);die;
+        //绑定ip和端口号
+        socket_bind($this->master, $address, $port)or die("socket_bind() failed");
+        while (true){
+            $from = '';
+            $port = 0;
+            socket_recvfrom($this->master,$buf,1024,0,$from,$port);
+            echo $buf;
+            usleep(1000);
+        }
     }
 
 
